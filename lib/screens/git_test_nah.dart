e6696d7c4d68c13aad369214f64e701e1273fcf6
AutoGGerr <217340750+AutoGGerr@users.noreply.github.com>
2026-02-18 16:30:53 +0300

    Add too screen

---
+++ lib/screens/git_test_nah.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GITHUB extends StatelessWidget {
+  const GITHUB({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      child: Text("заебааал гитхаааб"),
+    );
+  }
+}
